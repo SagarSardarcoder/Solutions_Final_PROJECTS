8ce490ebefcdd735fc9c6982ac71c9fb7ef2d385
Rohitsch <contact.rohitsch@gmail.com>
2022-04-28 16:28:35 +0530

    phase-2.1

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const autherController = require('../controllers/autherController')
 let blogController = require('../controllers/blogController')
 
+//////////////////////phase-1 ///////////////////////
+
 router.post('/createAuther', autherController.authorCreate)
 router.post('/createBlog', blogController.createBlog)
 router.get('/getBlogs', blogController.getBlogs)
 router.put('/blogs/:blogId', blogController.putBlogs)
 router.delete('/blogs/:blogId', blogController.deleted)
 router.delete('/blog', blogController.queryDeleted)
+
+//////////////////phase-2////////////////////
+
+router.post('/login', autherController.loginAuthor)
 
 
 
